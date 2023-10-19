944f33ea8ae415940090fea61a4a7fb03ce2784e
abxyz99 <41878489+azerdoum@users.noreply.github.com>
2023-10-19 16:17:37 -0400

    updated to most recent version

--- checkout.js
+++ checkout.js
@@ -17,3 +17,36 @@
     }, false)
   })
 })()
+// only allow one selection per menu option
+document.addEventListener('DOMContentLoaded', function() {
+    const categories = document.querySelectorAll('.category');
+
+    categories.forEach(function(category) {
+        category.addEventListener('change', function(e) {
+            if (e.target.type === 'checkbox') {
+                const sameCategoryCheckboxes = category.querySelectorAll(`input[type="checkbox"]`);
+                sameCategoryCheckboxes.forEach(function(checkbox) {
+                    if (checkbox !== e.target) {
+                        checkbox.checked = false;
+                    }
+                });
+            }
+        });
+    });
+});
+// Existing JavaScript for checkbox selection
+document.addEventListener('DOMContentLoaded', function() {
+  const categories = document.querySelectorAll('.category');
+  categories.forEach(function(category) {
+    category.addEventListener('change', function(e) {
+      if (e.target.type === 'checkbox') {
+        const sameCategoryCheckboxes = category.querySelectorAll(`input[type="checkbox"]`);
+        sameCategoryCheckboxes.forEach(function(checkbox) {
+          if (checkbox !== e.target) {
+            checkbox.checked = false;
+          }
+        });
+      }
+    });
+  });
+});
